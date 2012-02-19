e928dce63e539be82bdd414a0cfd418116a1943a
Jean Thomas <tibounise@gmail.com>
2012-02-19 14:41:59 +0100

    Modification du système de thèmes

--- functions.php
+++ functions.php
@@ -8,8 +8,6 @@ function getArticle($key,$database) {
 
 	$xml = new simpleXMLElement($content);
 
-	// $xml->author[0];
-
 	foreach ($xml->article as $output) {
 		if ($output->key == $key) {
 			return array(0 => $output->title, 1 => $output->content, 2 => $output->pubdate, 3 => $output->author);
@@ -43,6 +41,22 @@ function getSiteInfos($database) {
 	$xml = new simpleXMLElement($content);
 
 	return array(0 => $xml->title[0], 1 => $xml->url[0]);
+}
+
+function openFile($url) {
+	if (file_exists($url)) {
+		$file = fopen($url,"r");
+		while (!feof ($file))
+			{
+				$buffer = fgets($file, 4096);
+				$output = $output . $buffer;
+  			}
+  		fclose($file);
+  	}
+  	else {
+  		$output = false;
+  	}
+	return $output;
 }
 
 ?>
--- index.php
+++ index.php
@@ -2,35 +2,23 @@
 
 include "params.php";
 include "functions.php";
+include "themes/" . $actual_theme . "/theme.php";
+
+$site_infos = getSiteInfos($datafile_url);
 
 if (preg_match("#^[0-9]+$#", $_GET["article"])) {
 
-	$output = getSiteInfos($datafile_url);
+	$article = getArticle($_GET["article"], $datafile_url);
 
-	$title_site = $output[0];
-
-	$output = getArticle($_GET["article"], $datafile_url);
-
-	$article_title = $output[0];
-	$article_content = html_entity_decode($output[1]);
-	$article_pubdate = $output[2];
-	$article_author = $output[3];
-
-	include "themes/" . $actual_theme . "/article.html";
+	echo showArticle($site_infos[0],$navigation_bar_content,$article[0],$article[2],$article[3],$article[1]);
 
 }
 
 else {
 
-	$output = getSiteInfos($datafile_url);
+	$page = getPage("1", $datafile_url);
 
-	$title_site = $output[0];
-
-	$output = getPage("1", $datafile_url);
-
-	$article_content = html_entity_decode($output[0]);
-
-	include "themes/" . $actual_theme . "/index.html";
+	echo showPage($site_infos[0],$navigation_bar_content,$page[0]);
 
 }
 
