32aec8fbe6bc5989f5212d30ec625bc9a7372745
ayme32 <ayme32@me.com>
2012-02-20 14:39:02 +0100

    Ajout partiel de la partie admin des pages
    Pour faire des pages

--- admin/edit.php
+++ admin/edit.php
@@ -135,6 +135,125 @@
 				$page = "<p>You havn't filled some fields.</p><p><a href=\"index.php\" class=\"btn btn-warning\">Return to index</a></p>";
 			}
 		}
+		
+		elseif ($action == "list_page") {
+			$xml = new simpleXMLElement(file_get_contents("../" . $datafile_url));
+
+			$page = "<h1>Choose an page to edit.</h1><table class=\"table\"><thead><tr><th>#</th><th>Content</th></tr></thead><tbody>";
+
+			foreach ($xml->page as $output) {
+				$page .= "<tr>";
+				$page .= "<td>" . $output->key . "</td>";
+				$page .= "<td><a href=\"edit.php?action=edit_page&page=". $output->key ."\">" . substr(html_entity_decode($output->content), 0, 140) . "</a></td>";
+				$page .= "</tr>";
+			}
+
+			$page .= "</tbody></table>";
+		}
+		elseif ($action == "edit_page" AND !empty($_GET['page']) AND preg_match("#^[0-9]+$#", $_GET["page"])) {
+			$article = getPage($_GET['page'],"../".$datafile_url);
+			$page = "<h1>Edit an page</h1><br /><form class=\"form-horizontal\" action=\"edit.php?action=edit_article_processing\" method=\"POST\"><fieldset>";
+			$page .= "<input type=\"hidden\" name=\"id\" value=\"" . $_GET['page'] . "\">";
+			$page .= "<div class=\"control-group\"><label class=\"control-label\">Content : </label><div class=\"controls\"><textarea name=\"content\" class=\"span6\" rows=\"15\">" . html_entity_decode($page[0]) . "</textarea></div></div>";
+			$page .= "<div class=\"control-group\"><div class=\"controls\"><button type=\"submit\" class=\"btn btn-success\">Save changes</button></div></div>";
+			$page .= "</fieldset></form>";
+		}
+		elseif ($action == "edit_page_processing" AND isset($_POST['id']) AND isset($_POST['content'])) {
+			$xml = new simpleXMLElement(file_get_contents("../" . $datafile_url));
+
+			foreach ($xml->page as $output) {
+				if ($output->key == $_POST['id']) {
+					$output->content = htmlentities($_POST['content']);
+				}
+			}
+			
+			$buffer = $xml->asXML();
+			unlink("../" . $datafile_url);
+			$file = fopen("../" . $datafile_url,"w");
+			fputs($file,$buffer);
+			fclose($file);
+			$page = "<p>Your changes has been done !</p><p><a href=\"index.php\" class=\"btn btn-warning\">Return to index</a></p>";
+		}
+		elseif ($action == "delete_page") {
+			$xml = new simpleXMLElement(file_get_contents("../" . $datafile_url));
+
+			$page = "<h1>Choose an page to delete it.</h1><table class=\"table\"><thead><tr><th class=\"span1\"></th><th>#</th><th>Content</th></tr></thead><tbody>";
+
+			foreach ($xml->page as $output) {
+				$page .= "<tr>";
+				$page .= "<td><a style=\"margin-top: -3px;\" class=\"close\" href=\"edit.php?action=delete_page_confirmation&article=". $output->key ."\">&times;</a></td>";
+				$page .= "<td>" . $output->key . "</td>";
+				$page .= "<td><a href=\"edit.php?action=delete_page_confirmation&page=". $output->key ."\">" . html_entity_decode($output->content) . "</a></td>";
+				$page .= "</tr>";
+			}
+
+			$page .= "</tbody></table>";
+		}
+		elseif ($action == "delete_page_confirmation" AND !empty($_GET['page'])) {
+
+			$page = "<h1>Are you sure that you want to delete it ?</h1><br />";
+			$page .= "<a class=\"btn btn-danger\" href=\"edit.php?action=delete_page_processing&article=" . $_GET['page'] . "\">Continue</a>&nbsp;";
+			$page .= "<a class=\"btn btn-success\" href=\"index.php\">Cancel</a>";
+		}
+		elseif ($action == "delete_page_processing" AND !empty($_GET['page'])) {
+			$xml = new simpleXMLElement(file_get_contents("../" . $datafile_url));
+
+			$i = 0;
+
+			foreach ($xml->article as $output) {
+				if ($output->key == $_GET['article']) {
+					unset($xml->article[$i]); break;
+				}
+				$i++;
+			}
+
+			$buffer = $xml->asXML();
+			unlink("../" . $datafile_url);
+			$file = fopen("../" . $datafile_url,"w");
+			fputs($file,$buffer);
+			fclose($file);
+
+			$page = "<p>Article deleted.</p><p><a href=\"index.php\" class=\"btn btn-warning\">Return to index</a></p>";
+		}
+		elseif ($action == "add_article") {
+			$page = "<h1>Add an article</h1><br /><form class=\"form-horizontal\" action=\"edit.php?action=add_article_processing\" method=\"POST\"><fieldset>";
+			$page .= "<div class=\"control-group\"><label class=\"control-label\">Name of the article : </label><div class=\"controls\"><input type=\"text\" class=\"span6\" id=\"title\" placeholder=\"Title\" name=\"title\"></div></div>";
+			$page .= "<div class=\"control-group\"><label class=\"control-label\">Author : </label><div class=\"controls\"><input type=\"text\" class=\"span6\" id=\"title\" placeholder=\"Author\" name=\"author\"></div></div>";
+			$page .= "<div class=\"control-group\"><label class=\"control-label\">Pubdate : </label><div class=\"controls\"><input type=\"text\" class=\"span6\" id=\"title\" placeholder=\"Pubdate\" name=\"pubdate\"></div></div>";
+			$page .= "<div class=\"control-group\"><label class=\"control-label\">Content : </label><div class=\"controls\"><textarea name=\"content\" class=\"span6\" rows=\"15\" placeholder=\"Type something here.\"></textarea></div></div>";
+			$page .= "<div class=\"control-group\"><div class=\"controls\"><button type=\"submit\" class=\"btn btn-success\">Save</button></div></div>";
+			$page .= "</fieldset></form>";
+		}
+
+		elseif ($action == "add_article_processing") {
+			if (!empty($_POST['title']) AND !empty($_POST['author']) AND !empty($_POST['content']) AND !empty($_POST['pubdate'])) {	
+				$xml = new simpleXMLElement(file_get_contents("../" . $datafile_url));
+				
+				$id = 1;
+
+				foreach ($xml->article as $output) {
+					if ($output->key >= $id) {
+						$id = $output->key + 1;
+					}
+				}
+
+				$article = $xml->addChild("article","");
+				$article->addChild("key",$id);
+				$article->addChild("author",$_POST['author']);
+				$article->addChild("title",htmlentities($_POST['title']));
+				$article->addChild("pubdate",$_POST['pubdate']);
+				$article->addChild("content",htmlentities($_POST['content']));
+				$buffer = $xml->asXML();
+				unlink("../" . $datafile_url);
+				$file = fopen("../" . $datafile_url,"w");
+				fputs($file,$buffer);
+				fclose($file);
+				$page = "<p>The article has been published.</p><p><a href=\"index.php\" class=\"btn btn-warning\">Return to index</a></p>";
+			}
+			else {
+				$page = "<p>You havn't filled some fields.</p><p><a href=\"index.php\" class=\"btn btn-warning\">Return to index</a></p>";
+			}
+		}
 
 		include("design.html");
 	}
