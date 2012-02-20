5babe19d43c867b1cff5cfe2e2d23c893d0d31e5
ayme32 <ayme32@me.com>
2012-02-20 16:01:44 +0100

    Ajout de la partie admin de la config du site
    Pour configurer le site

--- admin/config.php
+++ admin/config.php
@@ -6,18 +6,18 @@
 	include("../functions.php");
 
 	if (isset($_SESSION['connected']) AND $_SESSION['connected'] == true) {
-		if (empty($_GET['edit_config_processing'])) {
+		if (!isset($_GET['action'])) {
 			$config_get = getSiteInfos("../".$datafile_url);
-			$page = "<h1>Edit config</h1><br /><form class=\"form-horizontal\" action=\"edit.php?action=edit_config_processing\" method=\"POST\"><fieldset>";
-			$page .= "<div class=\"control-group\"><label class=\"control-label\">Name of the site : </label><div class=\"controls\"><input type=\"text\" class=\"span6\" id=\"title\" value=\"" . html_entity_decode($config_get[0]) . "\" name=\"title\"></div></div>";
+			$page = "<h1>Edit config</h1><br /><form class=\"form-horizontal\" action=\"config.php?action=edit_config_processing\" method=\"POST\"><fieldset>";
+			$page .= "<div class=\"control-group\"><label class=\"control-label\">Name of the site : </label><div class=\"controls\"><input type=\"text\" class=\"span6\" id=\"name\" value=\"" . html_entity_decode($config_get[0]) . "\" name=\"name\"></div></div>";
 			$page .= "<div class=\"control-group\"><div class=\"controls\"><button type=\"submit\" class=\"btn btn-success\">Save changes</button></div></div>";
 			$page .= "</fieldset></form>";
 		}
-		elseif ($action == "edit_config_processing" AND isset($_POST['title'])) {
+		elseif ($_GET['action'] == "edit_config_processing" AND isset($_POST['name'])) {
 			$xml = new simpleXMLElement(file_get_contents("../" . $datafile_url));
 
 			foreach ($xml->config as $output) {
-				$output->title = htmlentities($_POST['title']);
+				$output->title = htmlentities($_POST['name']);
 			}
 			
 			$buffer = $xml->asXML();
