b6eab7b920e1628fcdbf813812572656037f17bb
Jean Thomas <tibounise@gmail.com>
2012-02-20 16:03:30 +0100

    Changements d'apparence

--- admin/config.php
+++ admin/config.php
@@ -10,7 +10,7 @@
 			$config_get = getSiteInfos("../".$datafile_url);
 			$page = "<h1>Edit config</h1><br /><form class=\"form-horizontal\" action=\"config.php?action=edit_config_processing\" method=\"POST\"><fieldset>";
 			$page .= "<div class=\"control-group\"><label class=\"control-label\">Name of the site : </label><div class=\"controls\"><input type=\"text\" class=\"span6\" id=\"name\" value=\"" . html_entity_decode($config_get[0]) . "\" name=\"name\"></div></div>";
-			$page .= "<div class=\"control-group\"><div class=\"controls\"><button type=\"submit\" class=\"btn btn-success\">Save changes</button></div></div>";
+			$page .= "<div class=\"control-group\"><div class=\"controls\"><button type=\"submit\" class=\"btn btn-info\">Save changes</button></div></div>";
 			$page .= "</fieldset></form>";
 		}
 		elseif ($_GET['action'] == "edit_config_processing" AND isset($_POST['name'])) {
